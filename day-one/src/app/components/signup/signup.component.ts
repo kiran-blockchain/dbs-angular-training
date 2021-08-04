32ce0e8633945e3e5f1f0067221433184405e00c
crypto <crypto@cryptos-MBP.Dlink>
2021-08-04 16:19:38 +0530

    Added country list and state list

--- day-one/src/app/components/signup/signup.component.ts
+++ day-one/src/app/components/signup/signup.component.ts
@@ -6,15 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-   UserDetails:any;
+  UserDetails: any;
+  CountryList: any;
+  StateList:any;
   constructor() {
-    this.UserDetails={}
-   }
-   signup(){
-     console.log(this.UserDetails);
-     this.UserDetails.Age=50;
-   }
+    this.UserDetails = {
+      gender: "M",
+      Country: "USA"
+    };
+    this.CountryList = [{
+      name: "India", value: "IN",
+      states: [{ name: "Telangana", value: "TS" },
+      { name: "Andhra Pradesh", value: "AP" }
+      ]
+    },
+    {
+      name: "United States",
+      value: "USA",
+      states: [{
+        name: "New York",
+        value: "NY",
+      },
+      {
+        name: "New Jersy",
+        value: "NJ",
+      }
+      ]
+    }]
+  }
+  signup() {
+    console.log(this.UserDetails);
+    this.UserDetails.Age = 50;
+  }
+  loadStates(){
+    for(let i=0;i<this.CountryList.length;i++){
+      if(this.CountryList[i].value== this.UserDetails.Country){
+        this.StateList = this.CountryList[i].states;
+        break;
+      }
+    }
+  }
 
- 
 
 }
