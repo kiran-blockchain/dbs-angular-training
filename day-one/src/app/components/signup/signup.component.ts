dffe7261dd5a0c9055bb0bb4a061b3d4b118d6c1
crypto <crypto@cryptos-MBP.Dlink>
2021-08-05 09:43:45 +0530

    Added Signup Form

--- day-one/src/app/components/signup/signup.component.ts
+++ day-one/src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserProfile } from 'src/app/models/UserProfile';
 
 @Component({
   selector: 'app-signup',
@@ -6,46 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-  UserDetails: any;
-  CountryList: any;
-  StateList:any;
-  constructor() {
-    this.UserDetails = {
-      gender: "M",
-      Country: "USA"
-    };
-    this.CountryList = [{
-      name: "India", value: "IN",
-      states: [{ name: "Telangana", value: "TS" },
-      { name: "Andhra Pradesh", value: "AP" }
-      ]
-    },
-    {
-      name: "United States",
-      value: "USA",
-      states: [{
-        name: "New York",
-        value: "NY",
-      },
-      {
-        name: "New Jersy",
-        value: "NJ",
-      }
-      ]
-    }]
+  userProfile:UserProfile;
+  constructor(){
+    this.userProfile = new UserProfile();
   }
-  signup() {
-    console.log(this.UserDetails);
-    this.UserDetails.Age = 50;
+  showSignUp(){
+    return this.userProfile.AgreeTerms;
   }
-  loadStates(){
-    for(let i=0;i<this.CountryList.length;i++){
-      if(this.CountryList[i].value== this.UserDetails.Country){
-        this.StateList = this.CountryList[i].states;
-        break;
-      }
-    }
+  disableSingup(){
+    return !this.userProfile.AgreeTerms;
   }
-
-
+    
 }
---
+++ day-one/src/app/models/UserProfile.ts
@@ -0,0 +1,13 @@
+export class UserProfile {
+    FirstName: string="";
+    LastName: string='';
+    UserName:string='';
+    Email:string='';
+    Age:Number=0;
+    Gender: string='';
+    DateOfBirth: string='';
+    Country: string='';
+    Password: string='';
+    ConfirmPassword: string='';
+    AgreeTerms: string='';
+}
